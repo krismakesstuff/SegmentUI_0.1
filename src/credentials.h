06e25c3a65f24be273b7d62f2258816155eec81f
Kris Crawford <11crawfordk@gmail.com>
2024-10-02 12:06:59 -0400

    Update credentials.h

--- src/credentials.h
+++ src/credentials.h
@@ -1,3 +1,5 @@
+
+
 // WIFI credentials. CHANGE THESE TO YOUR NETWORK SSID AND PASSWORD
-#define CONFIG_WIFI_SSID "spaceballs"
-#define CONFIG_WIFI_PASSWORD "pewpew!69"
+#define CONFIG_WIFI_SSID "your-ssid"
+#define CONFIG_WIFI_PASSWORD "your-password"
